1fc8be2fa38abaa2c33c2f33270045ff757a3ec8
Souleymane Diallo <soultaka4229@gmail.com>
2026-01-26 14:00:44 -0500

    erff

--- src/app/layout/sidebar/sidebar-header.ts
+++ src/app/layout/sidebar/sidebar-header.ts
@@ -18,8 +18,15 @@ import { ButtonModule } from 'primeng/button';
             <p class="text-xs text-primary-600/70 font-medium whitespace-nowrap">Gestion Interventions</p>
           </div>
         </div>
+        <button
+        pButton
+        type="button"
+        icon="pi pi-angle-left"
+        class="p-button-text p-button-rounded p-button-sm !text-primary-600 hover:!text-primary-700 hover:!bg-primary-50 transition-colors hidden md:flex"
+        (click)="toggle.emit()"
+      ></button>
       } @else {
-        <div class="w-10 h-10 bg-gradient-to-br from-primary-600 to-primary-700 rounded-xl flex items-center justify-center flex-shrink-0 shadow-lg ring-2 ring-primary-100 mx-auto">
+        <div class="w-10 h-10  flex items-center justify-center mx-auto">
         <button
             pButton
             type="button"
@@ -27,17 +34,9 @@ import { ButtonModule } from 'primeng/button';
             class="p-button-text p-button-rounded md:hidden !text-primary-600 hover:!text-primary-700 hover:!bg-primary-50 transition-colors"
             (click)="toggle.emit()"
           ></button>  
-        <!-- <i class="pi pi-wrench text-white text-xl"></i> -->
         </div>
       }
 
-      <button
-        pButton
-        type="button"
-        [icon]="isCollapsed() ? 'pi pi-angle-right' : 'pi pi-angle-left'"
-        class="p-button-text p-button-rounded p-button-sm !text-primary-600 hover:!text-primary-700 hover:!bg-primary-50 transition-colors hidden md:flex"
-        (click)="toggle.emit()"
-      ></button>
     </div>
   `,
 })
